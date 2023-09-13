d8b777e38c09ffc91516bd4471460b3c9314b100
chenshuo-op <44315363+chenshuo-op@users.noreply.github.com>
2023-09-14 01:35:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,18 @@
-# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
-# Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
-# git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
-# git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+#修改默认登录地址
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#修改wifi信息
+sed -i 's/OpenWrt/qwerts/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/none/psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/psk2/a\set wireless.default_radio${devidx}.key='pikapika1987.'' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#添加定时重启插件
+sed -i '/luci-lib-fs/a\luci-app-autoreboot \' include/target.mk
+sed -i 's/option week '3'/option week '7'/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
+sed -i 's/option enable '0'/option enable '1'/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
+
+#关闭flow offload
+#sed -i 's/option syn_flood	1/option syn_flood	0/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
+
+#添加主题
+sed -i '/+luci-light/a\+luci-theme-argon \' include/target.mk
