dac8cabd9699039158c9d3d1e853a76c3e395b28
chenshuo-op <44315363+chenshuo-op@users.noreply.github.com>
2023-09-14 09:13:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,12 +2,12 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #修改wifi信息
-sed -i 's/OpenWrt/qwerts/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/qqq/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/none/psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i '/psk2/a\set wireless.default_radio${devidx}.key='pikapika1987.'' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/psk2/a\set wireless.default_radio${devidx}.key='123456789'' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #添加定时重启插件
-sed -i '/luci-lib-fs/a\luci-app-autoreboot \' include/target.mk
+sed -i '/luci-lib-fs/a \	luci-app-autoreboot \\' include/target.mk
 sed -i 's/option week '3'/option week '7'/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
 sed -i 's/option enable '0'/option enable '1'/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
 
