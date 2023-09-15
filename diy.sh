92c957d6504094e7fda3b0005ec71e47af6aa058
chenshuo-op <44315363+chenshuo-op@users.noreply.github.com>
2023-09-15 23:41:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,21 +2,28 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #修改wifi信息
-sed -i 's/OpenWrt/qqq/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/none/psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i '/psk2/a\set wireless.default_radio${devidx}.key='123456789'' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#5g
+sed -i 's/Channel=36/Channel=40/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/SSID1=OpenWrt/SSID1=qqq_5g/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/AuthMode=OPEN/AuthMode=WPA2PSK/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/EncrypType=NONE/EncrypType=AES/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i 's/WPAPSK1=12345678/WPAPSK1=987654321/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.5G.dat
+
+#2g
+sed -i 's/Channel=6/Channel=11/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.2G.dat
+sed -i 's/SSID1=OpenWrt/SSID1=qqq/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.2G.dat
+sed -i 's/AuthMode=OPEN/AuthMode=WPA2PSK/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.2G.dat
+sed -i 's/EncrypType=NONE/EncrypType=AES/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.2G.dat
+sed -i 's/WPAPSK1=12345678/WPAPSK1=987654321/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.2G.dat
 
 #添加定时重启插件
 sed -i '/luci-lib-fs/a \	luci-app-autoreboot \\' include/target.mk
-sed -i '/luci-lib-fs/a \	luci-app-filebrowser \\' include/target.mk
+#设置定时重启插件
 sed -i 's/3/7/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
 sed -i '6s/0/1/' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
-#sed ':a;N;$!ba;s/0/1/2' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
 
-
-#关闭flow offload
-#sed -i 's/option syn_flood	1/option syn_flood	0/g' package/network/config/firewall/files/firewall.config
-sed -i '0,/1/s/1/0/' package/network/config/firewall/files/firewall.config
+#关闭防火墙
+#sed -i '3s/1/0/' package/network/config/firewall/files/firewall.config
 
 #添加主题
 sed -i '/luci-light/a \	+luci-theme-argon \\' feeds/luci/collections/luci/Makefile
