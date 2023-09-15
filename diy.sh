5cc1821438b81d8650706d943d3401680166865a
chenshuo-op <44315363+chenshuo-op@users.noreply.github.com>
2023-09-15 19:02:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 #修改默认登录地址
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #修改wifi信息
 sed -i 's/OpenWrt/qqq/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -8,11 +8,11 @@ sed -i '/psk2/a\set wireless.default_radio${devidx}.key='123456789'' package/ker
 
 #添加定时重启插件
 sed -i '/luci-lib-fs/a \	luci-app-autoreboot \\' include/target.mk
-sed -i 's/option week '3'/option week '7'/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
+sed -i 's/3/7/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
 sed -i 's/option enable '0'/option enable '1'/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
 
 #关闭flow offload
-sed -i 's/option syn_flood	1/option syn_flood	0/g' feeds/luci/applications/luci-app-autoreboot/root/etc/config/autoreboot
+sed -i 's/option syn_flood	1/option syn_flood	0/g' files/package/network/config/firewall/files/firewall.config
 
 #添加主题
-sed -i '/+luci-light/a \	++luci-theme-argon \\' feeds/luci/collections/luci/Makefile
+sed -i '/luci-light/a \	+luci-theme-argon \\' feeds/luci/collections/luci/Makefile
