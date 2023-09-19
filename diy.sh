d80fea95b706199fdb88fc0bf373a06e381412cb
chenshuo-vi <84225266+chenshuo-vi@users.noreply.github.com>
2023-09-20 06:24:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -24,7 +24,6 @@ sed -i '1,/EncrypType=NONE/s/EncrypType=NONE/EncrypType=AES/' package/kernel/mt-
 sed -i 's/WPAPSK1=12345678/WPAPSK1=987654321/g' package/kernel/mt-drivers/mt_wifi/files/mt7603.dat
 
 #添加插件
-sed -i '/dropbear/d' include/target.mk
 #sed -i 's/luci-app-filetransfer/luci-app-autoreboot/g' include/target.mk
 sed -i '/luci-lib-fs/a \	luci-app-autoreboot \\' include/target.mk
 sed -i '/luci-lib-fs/a \	luci-app-filebrowser \\' include/target.mk
@@ -44,7 +43,7 @@ rm -rf target/linux/ramips/mt7621/base-files/etc/init.d/set-irq-affinity
 sed -i 's/loglevel:-5/loglevel:-9/g' package/utils/busybox/files/cron
 #删除wan6接口，关闭ipv6
 #sed -i '19,32d' package/network/services/ppp/files/lib/netifd/ppp6-up
-#sed -i '/dhcpv6/d' package/network/services/odhcpd/files/odhcpd.defaults
+sed -i '/dhcpv6/d' package/network/services/odhcpd/files/odhcpd.defaults
 sed -i '/uci/d' package/emortal/ipv6-helper/files/60-ipv6-hybrid
 
 #关闭防火墙
